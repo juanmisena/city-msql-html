7e123394c30add14a80f47a6529d212789485f27
juan <jdbetancourth22@misena.edu.co>
2022-07-15 23:23:53 -0500

    change in this files

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ const session = require('express-session');
 const app = express();
 const routerController = require('./routes/nation');
 require('dotenv').config({path: './src/.env'});
-const port = process.env.JPORT || 3000;
-const dbOptions = {host: process.env.JHOST, user: process.env.JUSER, password: process.env.JPASS, port: process.env.JPORTD, database: process.env.JDBS}
+const port = process.env.PORT || 3000;
+const dbOptions = {host: process.env.DB_HOST, user: process.env.DB_USER, password: process.env.DB_PASSWORD, port: process.env.DB_PORT, database: process.env.DB_DATABASE}
 app.use(morgan('dev'));
 app.use(myConnection(mysql, dbOptions, 'single'));
 app.use(session({resave: false, saveUninitialized: false, secret: 'secret'}));
